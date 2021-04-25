4f3e9a87073c01621ae6741834124b80285166e1
BCraine <brandoncraine193@gmail.com>
2021-04-25 17:15:13 -0400

    working on tests

--- tests/test_SantoreHW.py
+++ tests/test_SantoreHW.py
@@ -1,8 +1,9 @@
-# import main
+import main
 
 
-def test_happy(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "Massachusetts")
-    state = input("Please enter a state to do business in.\n"
-                  "You may pick between Massachusetts, New Hampshire, or Maine\n")
-    assert state == "Massachusetts"
+def test_happy():
+
+    result = main.main()
+    assert result == 77
+
+
